f34826278e939c1913fe712a710713b8f41fcc49
Ravi Pandey <67542571+Pandey9800@users.noreply.github.com>
2023-08-17 16:28:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -540,7 +540,12 @@ let a = document.getElementsByTagName('div')[0]
 
 //------------------------------------------------------------------------------
 
-first.insertAdjacentHTML('beforeend', '')
+// first.insertAdjacentHTML("beforebegin", "<div class='test'>beforebegin</div>")
+// first.insertAdjacentHTML("beforeend", "<div class='test'>beforeend</div>")
+// first.insertAdjacentHTML("afterbegin", "<div class='test'>afterbegin</div>")
+// first.insertAdjacentHTML("afterend", "<div class='test'>afterend</div>")
+
+//------------------------------------------------------------------------------
 
 
 
