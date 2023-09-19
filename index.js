f6878bea4d026a64a6a37c13cd93e7469d7178ba
Ravi Pandey <67542571+Pandey9800@users.noreply.github.com>
2023-09-19 18:35:37 +0530

    ...

--- index.js
+++ index.js
@@ -894,10 +894,11 @@ console.log(su)
 //------------------------------------------------------------------------------
 
 // Syncronous Programming
+p = document.getElementById('written')
 a = prompt('What is your Name?')
 b = prompt('What is your Age?')
 c = prompt('What is your favorite Color?')
-console.log(a + ' is ' + b + ' Years Old & Has ' + c + ' as favorite color.')
+(a + ' is ' + b + ' Years Old & Has ' + c + ' as favorite color.')
 
 
 //Asyncronous Programming
