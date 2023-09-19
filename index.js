bea226525f88f6e90569d0c6f502a854ee289eb9
Ravi Pandey <67542571+Pandey9800@users.noreply.github.com>
2023-09-19 18:40:27 +0530

    Sample Code

--- index.js
+++ index.js
@@ -898,7 +898,7 @@ p = document.getElementById('written')
 a = prompt('What is your Name?')
 b = prompt('What is your Age?')
 c = prompt('What is your favorite Color?')
-(a + ' is ' + b + ' Years Old & Has ' + c + ' as favorite color.')
+p.textContent = a + ' is ' + b + ' Years Old & Has ' + c + ' as favorite color.'
 
 
 //Asyncronous Programming
