96549100422aac17502b313ea14697fdfb8e1489
Ravi Pandey <67542571+Pandey9800@users.noreply.github.com>
2023-10-27 10:56:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -417,7 +417,7 @@ console.log(su)
 let ct = document.querySelectorAll('.card-title')
 ct[0].style.color = 'red'
 ct[1].style.color = 'blue'
-// ct[2].style.color = 'green'
+ct[2].style.color = 'green'
 
 // document.querySelector('.this').style.color = 'yellow'
 // document.querySelector('.this').style.background = 'darkblue'
